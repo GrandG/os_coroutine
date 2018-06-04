a96c5cbdac7662dc459a2a3de3e434b78ec2ebea
Wayne <616311401@qq.com>
2018-06-04 15:23:15 +0800

    一条线的pipeline

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Tags:
 - pipeline. 用generator实现pipeline. 即增加一个过滤器, 新增的内容只有包含过滤器的关键字才显示出来.
 - coroutine. 把grep由generator变成coroutine.
 - coroutine_decorator. 手动写coroutine装饰器, 使得coroutine可以直接使用.
+- exception. 捕抓close引起得异常
---
+++ pipeline.py
@@ -0,0 +1,41 @@
+"""
+用coroutine实现pipeline
+"""
+
+
+import time
+
+def coroutine(func):                            # 装饰器
+    def wrapper(*args, **kwargs):
+        c = func(*args, **kwargs)
+        c.send(None)
+        return c
+    return wrapper
+
+
+def follow(the_file, target):                  # pipeline的source, 用于源源不断地产生data, 这个通常不是coroutine
+    the_file.seek(0, 2)                        # source的形参必然包括下一个pipe的对象 这里是target. 必须用到send方法
+    while True:
+        line = the_file.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        target.send(line)
+
+@coroutine
+def grep(pattern, target):                    # filter. 属于source和sink之间的部分. 用于过滤
+    while True:                               # 他必须接受上一节的data(即=yield), 处理数据后, 把data send到下一节.
+        line = yield
+        if pattern in line:
+            target.send(line)
+
+@coroutine
+def printer():                                # sink, 用于最终接受数据, 并处理数据, 这个是coroutine
+    while True:                               # sink必须接受上一节pipe senf的data(即包含=yield), 但不包含下一节的pipe, 因为sink就是最后一节
+        line = yield
+        print(line)
+
+
+if __name__ == '__main__':
+    with open('log.log') as f:
+        follow(f, grep('python', printer()))
